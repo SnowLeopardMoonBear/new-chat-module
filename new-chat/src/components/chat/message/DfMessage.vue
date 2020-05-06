78cb95fa2efdc8b8261d09720241e3ea6bfa7d74
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-06 21:50:49 +0900

    reshape message box

--- new-chat/src/components/chat/message/DfMessage.vue
+++ new-chat/src/components/chat/message/DfMessage.vue
@@ -16,6 +16,7 @@ export default {
     /* width:100%; */
 }
 .df-message {
+    position:relative;
   color: rgb(85, 85, 85);
   /* float: left; */
   text-align:left;
@@ -23,8 +24,18 @@ export default {
   border-radius: 5px;
   background-color: rgb(255, 253, 112);
   margin-top: 5px;
-  margin-left: 5px;
+  margin-left: 15px;
   margin-right: 5px;
   padding: 3px;
+}
+.df-message:after {
+ border-top:10px solid rgb(255, 253, 112);
+ border-left: 10px solid transparent;
+ border-right: 0px solid transparent;
+ border-bottom: 0px solid transparent;
+ content:"";
+ position:absolute;
+ top:5px;
+ left:-10px;
 }
 </style>
--- new-chat/src/components/chat/message/UserMessage.vue
+++ new-chat/src/components/chat/message/UserMessage.vue
@@ -13,6 +13,7 @@ export default {
 <style>
 .user-message {
   color: rgb(85, 85, 85);
+  position:relative;
   text-align:right;
   width:50%;
   float: right;
@@ -20,8 +21,18 @@ export default {
   border-radius: 5px;
   margin-top: 5px;
   margin-left: 5px;
-  margin-right: 5px;
+  margin-right: 15px;
   padding: 3px;
+}
+.user-message:after {
+ border-top:10px solid rgb(143, 243, 129);
+ border-left: 0px solid transparent;
+ border-right: 10px solid transparent;
+ border-bottom: 0px solid transparent;
+ content:"";
+ position:absolute;
+ top:5px;
+ right:-10px;
 }
 .message-line{
   /* width:100%; */
